63273712215b361c5966641a386985219ffbdf81
zwj <841752349@qq.com>
2018-12-19 11:04:15 +0800

    Signed-off-by: zwj <841752349@qq.com>

---
+++ chengyuan.py
@@ -0,0 +1,11 @@
+class A():
+    def __init__(self, a=0, b=0):
+        self._a = 0
+        self.__b = 0
+    def zz(self):
+        print("123460")
+
+c = A()
+# print(c._a)
+print(c._A__b)
+c.zz()
